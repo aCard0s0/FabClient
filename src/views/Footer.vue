110adb85471f04badae55282cbbb22b42af088e2
cardosoa2 <72450670+cardosoa2@users.noreply.github.com>
2022-01-25 18:21:01 +0000

    Footer fixed

--- src/views/Footer.vue
+++ src/views/Footer.vue
@@ -1,77 +1,63 @@
 <template>
-  <!-- Footer -->
   <footer class="text-center text-lg-start bg-light text-muted">
-    <!-- Section: Social media
-    <section class="d-flex justify-content-center justify-content-lg-between border-bottom" p- >
-      <div class="me-5 d-none d-lg-block">
-        <span>Get connected with us on social networks:</span>
-      </div>
 
-      <div>
-        <a href="" class="me-4 text-reset">
-          <i class="fab fa-facebook-f"></i>
-        </a>
-        <a href="" class="me-4 text-reset">
-          <i class="fab fa-twitter"></i>
-        </a>
-        <a href="" class="me-4 text-reset">
-          <i class="fab fa-google"></i>
-        </a>
-        <a href="" class="me-4 text-reset">
-          <i class="fab fa-instagram"></i>
-        </a>
-        <a href="" class="me-4 text-reset">
-          <i class="fab fa-linkedin"></i>
-        </a>
-        <a href="" class="me-4 text-reset">
-          <i class="fab fa-github"></i>
-        </a>
-      </div>
-    </section>-->
-
-    <!-- Section: Links  -->
     <section class="p-2">
       <div class="container text-center text-md-start mt-3">
         <div class="row mt-3">
-          <div class="col-3 mx-auto mb-4">
-            <h6 class="text-uppercase fw-bold">
-              Fable Deck
-            </h6>
-            <p>
-              Made by players to players
-            </p>
-          </div>
 
-          <div class="col-5 mx-auto mb-4">
-            <h6 class="text-uppercase fw-bold">
-              Donate
-            </h6>
-            <div style="font-size: 14px">
-              <p>
-                <b>BTC</b>: bc1qstd8re32lcvmnwlu6cl2ac42kp42z2a75utnue
-                <br />
-                <b>ETH:</b> 0x2a7D2c548A473274978D457d55461ceD46583DF4
-                <br />
-                <b>ADA:</b> Ae2tdPwUPEZ65cQZgEojjkpKeSAVMyFmj1sznkvTaJeEV88YBjnuF2EK7mD
-                <br />
-                <b>XRP:</b> rHkh4eoSmboLzjnvMTAf65tsjZnHWEDn39
-              </p>
-            </div>
-          </div>
-
-          <div class="col-2 mb-md-0 mb-4">
-            <h6 class="text-uppercase fw-bold">
+          <!-- Contact & Social Media -->
+          <div class="col-6 ">
+            <h6 class="text-uppercase fw-bold text-gold">
               Contact
             </h6>
             <p>
               <i class="fas fa-envelope me-3"></i>
               <font-awesome-icon icon="envelope" /> fabledeck@gmail.com
             </p>
+            <!--
+            <div>
+              <a href="" class="me-4 text-reset">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+              <a href="" class="me-4 text-reset">
+                <i class="fab fa-twitter"></i>
+              </a>
+              <a href="" class="me-4 text-reset">
+                <i class="fab fa-google"></i>
+              </a>
+              <a href="" class="me-4 text-reset">
+                <i class="fab fa-instagram"></i>
+              </a>
+              <a href="" class="me-4 text-reset">
+                <i class="fab fa-linkedin"></i>
+              </a>
+              <a href="" class="me-4 text-reset">
+                <i class="fab fa-github"></i>
+              </a>
+            </div>
+            -->
           </div>
+
+          <div class="col-6 ">
+            <h6 class="text-uppercase fw-bold text-gold">
+              Donate
+            </h6>
+            <div class="left-margin" style="font-size: 14px">
+              <p>
+                <b >BTC:</b> <code>bc1qstd8re32lcvmnwlu6cl2ac42kp42z2a75utnue</code>
+                <br />
+                <b>ETH:</b> <code>0x2a7D2c548A473274978D457d55461ceD46583DF4</code>
+                <br />
+                <b>ADA:</b> <code>Ae2tdPwUPEZ65cQZgEojjkpKeSAVMyFmj1sznkvTaJeEV88YBjnuF2EK7mD</code>
+                <br />
+                <b>XRP:</b> <code>rHkh4eoSmboLzjnvMTAf65tsjZnHWEDn39</code>
+              </p>
+            </div>
+          </div>
+
         </div>
       </div>
     </section>
-
   </footer>
 </template>
 
@@ -86,5 +72,11 @@ footer {
   position: relative;
   bottom: 0;
   width: 100%;
+}
+code {
+  color: inherit !important;
+}
+.left-margin {
+  margin-left: 1rem;
 }
 </style>
